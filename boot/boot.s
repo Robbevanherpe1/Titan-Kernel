4117ed61ea3c92913b3c2ba6d25a5b8ef46634e3
Robbe <robbe.van.herpe@outlook.com>
2024-07-19 13:29:49 +0200

    add boot

--- boot/boot.s
+++ boot/boot.s
@@ -1,29 +1,42 @@
 BITS 32
+# start address for the kernel
 [org 0x7c00]
 
-mov ax, 0x10
-mov ds, ax
-mov es, ax
-mov fs, ax
-mov gs, ax
-mov ss, ax
-mov esp, 0x9fc00
+start:
+    # set up the stack pointer
+    mov ax, 0x07C0
+    add ax, 288
+    mov ss, ax
+    mov sp, 4096
 
-call start_kernel
+    # set up the segments
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-cli
-hlt
+    call start_kernel
+
+    # halt the computer
+    cli
+    hlt
 
 start_kernel:
+    # set up the stack pointer
     mov ebx, kernel_stack
     mov esp, ebx
+
+    # jump to the kernel
     extern kernel_main
     call kernel_main
 .hang:
     jmp .hang
 
+# the kernel stack
 kernel_stack:
     times 512 db 0
 
+# padding to make the file 512 bytes
 times 510-($-$$) db 0
 dw 0xaa55

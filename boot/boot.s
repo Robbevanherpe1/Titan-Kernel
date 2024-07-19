d6f5ad0a94d444c6f7a250c6130d4f1ef7a70f12
Robbe <robbe.van.herpe@outlook.com>
2024-07-19 15:16:34 +0200

    fix boot

--- boot/boot.s
+++ boot/boot.s
@@ -1,42 +1,71 @@
-BITS 32
-# start address for the kernel
+BITS 16
 [org 0x7c00]
 
 start:
-    # set up the stack pointer
-    mov ax, 0x07C0
-    add ax, 288
+    ; Set up the stack
+    cli
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
     mov ss, ax
-    mov sp, 4096
+    mov sp, 0x7c00
 
-    # set up the segments
+    ; Load GDT
+    lgdt [gdt_descriptor]
+
+    ; Enter protected mode
+    mov eax, cr0
+    or eax, 1
+    mov cr0, eax
+
+    ; Far jump to flush the prefetch queue
+    jmp 08h:protected_mode
+
+gdt_start:
+    ; Null descriptor
+    dq 0
+
+    ; Code segment descriptor
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    ; Data segment descriptor
+    dw 0xffff
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+BITS 32
+protected_mode:
+    ; Set up data segments
     mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
+    mov ss, ax
+    mov esp, 0x90000
 
-    call start_kernel
+    ; Jump to the kernel entry point
+    jmp 0x08:kernel_entry
 
-    # halt the computer
-    cli
-    hlt
-
-start_kernel:
-    # set up the stack pointer
-    mov ebx, kernel_stack
-    mov esp, ebx
-
-    # jump to the kernel
-    extern kernel_main
-    call kernel_main
+kernel_entry:
+    ; This is where your kernel code starts
+    ; For now, just loop
 .hang:
     jmp .hang
 
-# the kernel stack
-kernel_stack:
-    times 512 db 0
-
-# padding to make the file 512 bytes
+; Padding to make the file 512 bytes
 times 510-($-$$) db 0
 dw 0xaa55
